7bb91cf989e214dac304895f33e5082fa54c673d
João Victor <dante@archangel.localdomain>
2020-05-25 21:12:16 -0300

    :sparkles: Create Event Feature

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from core.models import Event
 
 # Register your models here.
+
+class EventAdmin(admin.ModelAdmin):
+  list_display = ('title', 'event_date', 'datetimestamp') 
+  list_filter = ('title', 'event_date',)
+
+admin.site.register(Event, EventAdmin)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Event(models.Model):
+  title = models.CharField(max_length=100)
+  desc = models.TextField(blank=True, null=True)
+  event_date = models.DateTimeField()
+  datetimestamp = models.DateTimeField(auto_now=True, verbose_name="Created")
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+  class Meta:
+    db_table = 'event'
+
+  def __str__(self):
+    return self.title
